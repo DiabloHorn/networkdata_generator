f3585470bf228db59a56a6f5a6d8ce06ac35e54b
DiabloHorn <diablohorn@gmail.com>
2022-09-04 23:52:18 +0200

    Proper formatting

--- generator.py
+++ generator.py
@@ -4,26 +4,47 @@
 import itertools
 
 
-from network.connections import vlan_ipv4_object
-from network.connections import vlan_ipv4_connections
+from network.connections import VlanIPv4Object
+from network.connections import VlanIPv4Connections
 
 
 if __name__ == "__main__":
-    #configtemplate = [{'vlanname':'clients','innercoverage':15,'outercoverage':25,'innerports':[((445,'smb'),(3389,'rdp'))],'outerports':[(80,'http'),(389,'ldap')]}]
-    VLAN_NAMES = ['clients','servers','dmz','databases','voip','admins','servers_windows','servers_linux']
+    # configtemplate = [
+    #     {
+    #         "vlanname": "clients",
+    #         "innercoverage": 15,
+    #         "outercoverage": 25,
+    #         "innerports": [((445, "smb"), (3389, "rdp"))],
+    #         "outerports": [(80, "http"), (389, "ldap")],
+    #     }
+    # ]
+    VLAN_NAMES = [
+        "clients",
+        "servers",
+        "dmz",
+        "databases",
+        "voip",
+        "admins",
+        "servers_windows",
+        "servers_linux",
+    ]
 
     for vln in VLAN_NAMES:
-        vlan = vlan_ipv4_object(vln)
+        vlan = VlanIPv4Object(vln)
 
-        vlaninnerconnections = vlan_ipv4_connections(vlan,vlan,[(445,'smb'),(3389,'rdp')])
+        vlaninnerconnections = VlanIPv4Connections(
+            vlan, vlan, [(445, "smb"), (3389, "rdp")]
+        )
         for conn in vlaninnerconnections.get_host_connections():
             print(conn)
 
     print("====VLAN TO VLAN CONNECTIONS====")
     for bla in itertools.permutations(VLAN_NAMES, 2):
-        srcvlan = vlan_ipv4_object(vln[0])
-        dstvlan = vlan_ipv4_object(vln[1])
+        srcvlan = VlanIPv4Object(vln[0])
+        dstvlan = VlanIPv4Object(vln[1])
 
-        vlaninnerconnections = vlan_ipv4_connections(srcvlan,dstvlan,[(445,'smb'),(3389,'rdp')],innerconnections=False)
+        vlaninnerconnections = VlanIPv4Connections(
+            srcvlan, dstvlan, [(445, "smb"), (3389, "rdp")], innerconnections=False
+        )
         for conn in vlaninnerconnections.get_host_connections():
             print(conn)
--- network/connections.py
+++ network/connections.py
@@ -5,19 +5,19 @@ import math
 import logging
 import ipaddress
 
-gnd_logger = logging.getLogger('networkdata_generator')
+gnd_logger = logging.getLogger("networkdata_generator")
 console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter('[%(asctime)s::%(filename)s::%(funcName)s] %(message)s',"%Y-%m-%d %H:%M:%S"))
+console_handler.setFormatter(
+    logging.Formatter(
+        "[%(asctime)s::%(filename)s::%(funcName)s] %(message)s", "%Y-%m-%d %H:%M:%S"
+    )
+)
 gnd_logger.addHandler(console_handler)
 gnd_logger.setLevel(logging.WARNING)
 
-class network_helper:
-    """Helper class with static methods
-    """
-    def __init__(self) -> None:
-        """Default empty init
-        """
-        pass
+
+class NetworkHelper:
+    """Helper class with static methods"""
 
     @staticmethod
     def get_src_port() -> int:
@@ -27,12 +27,19 @@ class network_helper:
             int: Source port number between 49152 and 65535
         """
         # https://docs.microsoft.com/en-GB/troubleshoot/windows-server/networking/default-dynamic-port-range-tcpip-chang
-        return random.randrange(49152,65535)
+        return random.randrange(49152, 65535)
 
-class vlan_ipv4_object:
-    """vlan ipv4 object to hold vlan related logic
-    """
-    def __init__(self,vlanname:str,networkmask:int=24,innercoverage:int=15,outercoverage:int=25) -> None:
+
+class VlanIPv4Object:
+    """vlan ipv4 object to hold vlan related logic"""
+
+    def __init__(
+        self,
+        vlanname: str,
+        networkmask: int = 24,
+        innercoverage: int = 15,
+        outercoverage: int = 25,
+    ) -> None:
         """Initializes vlan ipv4 object and generates required data
 
         Args:
@@ -50,16 +57,16 @@ class vlan_ipv4_object:
         self._gen_vlanrange(networkmask)
         self.vlan_numhosts = len(list(self.vlan_ipv4range.hosts()))
 
-    def _gen_vlanid(self,start:int=1,end:int=10000) -> None:
+    def _gen_vlanid(self, start: int = 1, end: int = 10000) -> None:
         """Generate a random vlan id
 
         Args:
             start (int, optional): Start of the range. Defaults to 1.
             end (int, optional): End of the range. Defaults to 10000.
         """
-        self.vlan_id = random.randint(start,end)
+        self.vlan_id = random.randint(start, end)
 
-    def _gen_vlanrange(self,mask:int) -> None:
+    def _gen_vlanrange(self, mask: int) -> None:
         """Generate the ipv4 range for the vlan
 
         Args:
@@ -67,12 +74,12 @@ class vlan_ipv4_object:
         """
         octets = []
         for _ in range(4):
-            octets.append(random.randint(0,255))
+            octets.append(random.randint(0, 255))
         rndip = f"{octets[0]}.{octets[1]}.{octets[2]}.{octets[3]}/{mask}"
         gnd_logger.debug("rndip %s", rndip)
-        self.vlan_ipv4range = ipaddress.IPv4Network(rndip,strict=False)
+        self.vlan_ipv4range = ipaddress.IPv4Network(rndip, strict=False)
 
-    def get_numhost_coverage(self, innercoverage:bool=True) -> int:
+    def get_numhost_coverage(self, innercoverage: bool = True) -> int:
         """Get the amount of hosts available based on the coverage percentage
 
         Args:
@@ -82,9 +89,13 @@ class vlan_ipv4_object:
             int: Amount of hosts available based on coverage percentage
         """
         if innercoverage:
-            hostcount = math.floor(((self.vlan_innercoverage/100)*self.vlan_numhosts))
+            hostcount = math.floor(
+                ((self.vlan_innercoverage / 100) * self.vlan_numhosts)
+            )
         else:
-            hostcount = math.floor(((self.vlan_outercoverage/100)*self.vlan_numhosts))
+            hostcount = math.floor(
+                ((self.vlan_outercoverage / 100) * self.vlan_numhosts)
+            )
         gnd_logger.debug("hostcount: %s", hostcount)
         return hostcount
 
@@ -95,7 +106,7 @@ class vlan_ipv4_object:
             ipaddress.IPv4Address: The selected host
         """
         allhosts = list(self.vlan_ipv4range.hosts())
-        rnd = random.randrange(1,self.vlan_numhosts)
+        rnd = random.randrange(1, self.vlan_numhosts)
         ipv4host = allhosts[rnd]
         gnd_logger.info("randomhost %s", ipv4host)
         return ipv4host
@@ -113,18 +124,25 @@ class vlan_ipv4_object:
 
         Returns:
             str: A dict representation of the class
-        """        
+        """
         return str(self.__dict__)
 
-class vlan_ipv4_connections:
-    """vlan ipv4 connection object to hold all connection generating logic
-    """
-    def __init__(self,srcvlan:vlan_ipv4_object,dstvlan:vlan_ipv4_object,dstports:list[tuple[int,str]],innerconnections:bool=True) -> None:
+
+class VlanIPv4Connections:
+    """vlan ipv4 connection object to hold all connection generating logic"""
+
+    def __init__(
+        self,
+        srcvlan: VlanIPv4Object,
+        dstvlan: VlanIPv4Object,
+        dstports: list[tuple[int, str]],
+        innerconnections: bool = True,
+    ) -> None:
         """Instantiate connection object fields
 
         Args:
-            srcvlan (vlan_ipv4_object): The source vlan
-            dstvlan (vlan_ipv4_object): The destination vlan, can be the same if innerconnections is set to True
+            srcvlan (VlanIPv4Object): The source vlan
+            dstvlan (VlanIPv4Object): The destination vlan, can be the same if innerconnections is set to True
             dstports (list[tuple[int,str]]): The list of ports to use for outgoing connection. List consists of tuples (portnumber:int, servicename:str)
             innerconnections (bool, optional): Determines if connections are generated between hosts IN the vlan or from hosts in the vlan to OTHER vlans. Defaults to True.
         """
@@ -144,9 +162,11 @@ class vlan_ipv4_connections:
         if self._innerconnections:
             totalhosts = self._srcvlan.get_numhost_coverage()
         else:
-            totalhosts = self._srcvlan.get_numhost_coverage(innercoverage=self._innerconnections)
+            totalhosts = self._srcvlan.get_numhost_coverage(
+                innercoverage=self._innerconnections
+            )
         gnd_logger.debug("totalhosts %s", totalhosts)
-        for _ in range(0,totalhosts):
+        for _ in range(0, totalhosts):
             srchost = self._srcvlan.get_rnd_ipv4host()
             if self._innerconnections:
                 dsthost = self._srcvlan.get_rnd_ipv4host()
@@ -154,21 +174,21 @@ class vlan_ipv4_connections:
                 dsthost = self._dstvlan.get_rnd_ipv4host()
             gnd_logger.debug("srchost %s", srchost)
             gnd_logger.debug("dsthost %s", dsthost)
-            while(srchost == dsthost):
+            while srchost == dsthost:
                 dsthost = self._dstvlan.get_rnd_ipv4host()
                 gnd_logger.debug("new dsthost %s", dsthost)
-            gnd_logger.info("%s %s",srchost,dsthost)
-            ipv4hosts.append((srchost,dsthost))
+            gnd_logger.info("%s %s", srchost, dsthost)
+            ipv4hosts.append((srchost, dsthost))
         return ipv4hosts
-   
-    def _get_dst_port(self) -> tuple[int,str]:
+
+    def _get_dst_port(self) -> tuple[int, str]:
         """Get a random port from the list of destination ports
 
         Returns:
             tuple[int,str]: The randomly chosen destination tuple
         """
         maxport = len(self._dstports)
-        rnd = random.randrange(0,maxport)
+        rnd = random.randrange(0, maxport)
         return self._dstports[rnd]
 
     def get_host_connections(self) -> dict:
@@ -179,8 +199,19 @@ class vlan_ipv4_connections:
         """
         hosts = self._get_host_pairs()
         for pair in hosts:
-            srcport = network_helper.get_src_port()
+            srcport = NetworkHelper.get_src_port()
             dstport = self._get_dst_port()[0]
-            connection = {"srchost":str(pair[0]),"dsthost":str(pair[1]),"srcport":srcport,"dstport":dstport}
-            gnd_logger.info("%s:%s %s:%s", connection['srchost'],connection['srcport'],connection['dsthost'],connection['dstport'])
-            yield(connection)
+            connection = {
+                "srchost": str(pair[0]),
+                "dsthost": str(pair[1]),
+                "srcport": srcport,
+                "dstport": dstport,
+            }
+            gnd_logger.info(
+                "%s:%s %s:%s",
+                connection["srchost"],
+                connection["srcport"],
+                connection["dsthost"],
+                connection["dstport"],
+            )
+            yield (connection)
